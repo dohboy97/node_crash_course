381065fa8f3761597a43c3f0966f0e802ff5b8ab
Connor <connor.doherty97@gmail.com>
2022-05-19 20:00:28 -0400

    os demo added

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -5,4 +5,16 @@ const path = require('path')
 fs.mkdir(path.join(__dirname, '/test'), {}, function(err){
     if(err) throw err
 
+
+    console.log('folder created...')
+});
+
+
+//create and write to file
+
+fs.writeFile(path.join(__dirname, 'hello.txt'), 'hello world!', function(err){
+    if(err) throw err
+
+
+    console.log('file written to')
 });
